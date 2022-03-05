fc67604c6c21392025e91e096fa86e85a261a893
atinydev <atinydeveloper@gmail.com>
2022-03-05 00:22:48 -0600

    place Symbols

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:http/http.dart' as http;
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -13,40 +17,70 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   MapboxMapController? mapController;
-  static const latLng = LatLng(37.810575, -122.477174);
+  static const center = LatLng(37.810575, -122.477174);
   static const streetStyle =
       'mapbox://styles/atinyflame/cl0d8zhwn004816qhocvzc2ch';
   static const cianStyle =
       'mapbox://styles/atinyflame/cl0d8x6lb000q14ntfnko7qzs';
 
-  var selectedStyle = streetStyle;
+  var selectedStyle = cianStyle;
 
   _onMapCreated(MapboxMapController controller) {
     mapController = controller;
+  }
+
+  void _onStyleLoaded() {
+    addImageFromAsset("assetImage", "assets/symbols/custom-icon.png");
+    addImageFromUrl(
+        "networkImage", Uri.parse("https://via.placeholder.com/50"));
+  }
+
+  /// Adds an asset image to the currently displayed style
+  Future<void> addImageFromAsset(String name, String assetName) async {
+    final ByteData bytes = await rootBundle.load(assetName);
+    final Uint8List list = bytes.buffer.asUint8List();
+    return mapController!.addImage(name, list);
+  }
+
+  /// Adds a network image to the currently displayed style
+  Future<void> addImageFromUrl(String name, Uri uri) async {
+    var response = await http.get(uri);
+    return mapController!.addImage(name, response.bodyBytes);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: MapboxMap(
+        onStyleLoadedCallback: _onStyleLoaded,
         styleString: selectedStyle,
         accessToken: dotenv.env['publicToken'],
         onMapCreated: _onMapCreated,
         initialCameraPosition: const CameraPosition(
-          target: latLng,
+          target: center,
           zoom: 14,
         ),
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
+          // Symbols
           FloatingActionButton(
             onPressed: () {
               setState(() {
-                mapController?.animateCamera(CameraUpdate.zoomOut());
+                mapController?.addSymbol(
+                  const SymbolOptions(
+                    // textColor: '#cccccc',
+                    geometry: center,
+                    textField: 'Symbol added here',
+                    iconImage: 'networkImage',
+                    // iconSize: 3,
+                    textOffset: Offset(0, 2),
+                  ),
+                );
               });
             },
-            child: const Icon(Icons.zoom_out),
+            child: const Icon(Icons.signpost_sharp),
           ),
           const SizedBox(
             height: 5,
@@ -62,6 +96,18 @@ class _MapScreenState extends State<MapScreen> {
           const SizedBox(
             height: 5,
           ),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                mapController?.animateCamera(CameraUpdate.zoomOut());
+              });
+            },
+            child: const Icon(Icons.zoom_out),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+
           // Change Style
           FloatingActionButton(
             onPressed: () {
@@ -70,7 +116,7 @@ class _MapScreenState extends State<MapScreen> {
                     selectedStyle == streetStyle ? cianStyle : streetStyle;
               });
             },
-            child: const Icon(Icons.add_to_home_screen),
+            child: const Icon(Icons.change_circle),
           ),
         ],
       ),
